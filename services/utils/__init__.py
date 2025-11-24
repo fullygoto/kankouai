52e7f28296d29a9ab2353ccb88c44cc43cde3cf7
fullygoto <info@fullygoto.com>
2025-11-25 08:35:05 +0900

    refactor: extract pure text helpers to services.utils.text

---
+++ services/utils/__init__.py
@@ -0,0 +1,21 @@
+from .text import (
+    LINE_SAFE_CHARS,
+    _first_int_in_text,
+    _norm_text_jp,
+    _split_for_line,
+    _split_for_messaging,
+    _split_lines_commas,
+    is_rentacar_query,
+    parse_latlng_any,
+)
+
+__all__ = [
+    "LINE_SAFE_CHARS",
+    "_first_int_in_text",
+    "_norm_text_jp",
+    "_split_for_line",
+    "_split_for_messaging",
+    "_split_lines_commas",
+    "is_rentacar_query",
+    "parse_latlng_any",
+]
